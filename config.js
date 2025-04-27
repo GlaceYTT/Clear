d09b9a9c70494d8c8edac58269616ca11840869a
GlaceYTT <official.glaceyt@gmail.com>
2025-04-27 18:33:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ module.exports = {
         id: '1091391355261034568',
         status: true,
         logChannelId: '1366033296831807538',
-        safeRoles: ['1366010820756242452', 'ROLE_ID_D'],  // Users with these roles won't be kicked
-        nonSafeRoles: ['1366010873352814603', 'ROLE_ID_B'],  // Users with these roles will be kicked unless they have safe roles
+        safeRoles: ['1339588497761243176 ',' 1363213365337526494',' 1096127001858945024', '1238586828479528990' , '1098005398763606108'],  // Users with these roles won't be kicked
+        nonSafeRoles: ['1096142348217491547', '1096141592366174250'],  // Users with these roles will be kicked unless they have safe roles
         duration: 15  //  (e.g., 15 for 15 days)
       }
     ]
